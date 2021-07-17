510514063e034ebba020cb35b83c9dcc3c2ce16c
adrian.mutize@qubedlab.com <adrian.mutize@qubedlab.com>
2021-07-17 15:19:47 +0200

    banks

--- src/utils/http-base.js
+++ src/utils/http-base.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log(global)
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8080/api/',
+    baseURL: 'http://localhost:8082/api/',
     headers: {
         Authorization: 'Bearer {token}'
     }
