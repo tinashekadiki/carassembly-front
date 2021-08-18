b15e18f5033f31dd571cf8e44bbb06a1d5230d5c
adrian.mutize@qubedlab.com <adrian.mutize@qubedlab.com>
2021-08-18 20:24:45 +0200

    service types

--- src/views/ServiceTypesPage.vue
+++ src/views/ServiceTypesPage.vue
@@ -1,48 +1,50 @@
 <template>
   <MainLayout title="Service Types">
-    <div class="card">
-      <div class="card-header">Create Service Types</div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-md-8">
-            <label>Service Type Name</label>
-            <input class="form-control" v-model="serviceType.serviceTypeName" placeholder="Service Type Name" required>
-          </div>
-          <div class="col-md-2 mt-30">
-            <button class="btn btn-facebook" @click="saveServiceType()">Save Service Type</button>
+    <Loader v-if="globalLoadingState"></Loader>
+    <div v-else>
+      <div class="card">
+        <div class="card-header">Create Service Types</div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-8">
+              <label>Service Type Name</label>
+              <input class="form-control" v-model="serviceType.serviceTypeName" placeholder="Service Type Name"
+                     required>
+            </div>
+            <div class="col-md-2 mt-30">
+              <button class="btn btn-facebook" @click="saveServiceType()">Save Service Type</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div class="card">
+        <div class="card-header">Service Types</div>
+        <div class="card-body">
+          <table class="table table-responsive-sm table-striped table-bordered">
+            <thead>
+            <tr>
+              <th>Service Type Name</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="serviceType in serviceTypes" :key="serviceType.id">
+              <td>{{ serviceType.serviceTypeName }}</td>
+              <td>
+                <button class="btn btn-success" @click="setEditing(serviceType)">
+                  <i class="fa fa-edit"></i>
+                </button>
 
-    <div class="card">
-      <div class="card-header">Service Types</div>
-      <div class="card-body">
-        <table class="table table-responsive-sm table-striped table-bordered">
-          <thead>
-          <tr>
-            <th>Service Type Name</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="serviceType in serviceTypes" :key="serviceType.id">
-            <td>{{ serviceType.serviceTypeName }}</td>
-            <td>
-              <button class="btn btn-success" @click="setEditing(serviceType)">
-                <i class="fa fa-edit"></i>
-              </button>
-
-              <button class="btn btn-danger" @click="deleteServiceType(serviceType.id)">
-                <i class="fa fa-remove"></i>
-              </button>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+                <button class="btn btn-danger" @click="deleteServiceType(serviceType.id)">
+                  <i class="fa fa-remove"></i>
+                </button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-
   </MainLayout>
 </template>
 
@@ -50,11 +52,13 @@
 import MainLayout from "../layouts/MainLayout";
 import global from "../utils/global";
 import {http} from "../utils/http-base";
+import Loader from "../components/Loader";
+
 export default {
-name: "ServiceTypePage",
-  components: {MainLayout},
-  mixins:[global],
-  data(){
+  name: "ServiceTypePage",
+  components: {Loader, MainLayout},
+  mixins: [global],
+  data() {
     return {
       serviceType: {
         serviceTypeName: '',
@@ -63,19 +67,19 @@ name: "ServiceTypePage",
     }
   },
   mounted() {
-   this.getAllServiceTypes();
+    this.getAllServiceTypes();
   },
   methods: {
-    setEditing(serviceType){
+    setEditing(serviceType) {
       this.serviceType = serviceType;
     },
-    saveServiceType(){
-      if (!this.serviceType.serviceTypeName){
+    saveServiceType() {
+      if (!this.serviceType.serviceTypeName) {
         this.showErrorMessage();
         return;
       }
       this.globalLoadingState = true;
-      if(this.serviceType.id){
+      if (this.serviceType.id) {
         http.put(`service-types/edit/${this.serviceType.id}`, this.serviceType).then(res => {
           console.log(res)
           this.showSuccessMessage()
@@ -84,20 +88,28 @@ name: "ServiceTypePage",
           this.showErrorMessage()
         }).finally(() => {
           this.getAllServiceTypes();
+          this.serviceType = {
+            serviceTypeName: '',
+            id: ''
+          }
         });
-      }else{
+      } else {
         http.post('service-types/create', this.serviceType).then(res => {
           console.log(res)
-         this.showSuccessMessage()
+          this.showSuccessMessage()
         }).catch(err => {
           this.showErrorMessage()
           console.log(err)
         }).finally(() => {
           this.getAllServiceTypes();
+          this.serviceType = {
+            serviceTypeName: '',
+            id: ''
+          }
         });
       }
     },
-    deleteServiceType(serviceTypeId){
+    deleteServiceType(serviceTypeId) {
       this.globalLoadingState = true;
       http.delete(`service-types/delete/${serviceTypeId}`).then(res => {
         console.log(res)
