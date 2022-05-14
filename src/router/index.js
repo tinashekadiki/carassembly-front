0bcde4b304f6da2c96ff3046b30e4e450e27ed39
Tinashe Kadiki <prof.kadiki@gmail.com>
2022-05-14 10:45:29 +0200

    Add express

--- src/router/index.js
+++ src/router/index.js
@@ -29,139 +29,139 @@ import DownloadJobCardPage from "../views/DownloadJobCardPage.vue"
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/dashboard',
-        name: "JobCardTable",
-        component: JobCardsPage
-    },
-    {
-        path: '/edit/:id',
-        name: "EditJobCard",
-        component: EditJobCard
-    },
-    {
-        path: '/create',
-        name: "NewJobCard",
-        component: NewJobCard
-    },
+    path: '/dashboard',
+    name: "JobCardTable",
+    component: JobCardsPage
+},
+{
+    path: '/edit/:id',
+    name: "EditJobCard",
+    component: EditJobCard
+},
+{
+    path: '/create',
+    name: "NewJobCard",
+    component: NewJobCard
+},
 
-    {
-        path: '/statuses/',
-        name: "StatusesPage",
-        component: StatusesPage
-    },
-    {
-        path: '/stocks',
-        name: "StockPage",
-        component: StockPage
-    },
-    {
-        path: '/stocks/create',
-        name: "NewStock",
-        component: NewStock
-    },
-    {
-        path: '/customers',
-        name: "CustomersPage",
-        component: CustomersPage
-    },
-    {
-        path: '/users',
-        name: "AdvisorsPage",
-        component: AdvisorsPage
-    },
-    {
-        path: '/jobCards/serviceTypes',
-        name: "ServiceTypePage",
-        component: ServiceTypePage
-    },
-    {
-        path: '/tax',
-        name: "TaxConfigList",
-        component: TaxConfigList
-    },
-    {
-        path: '/invoices',
-        name: "InvoicesListPage",
-        component: InvoicesListPage
-    },
-    {
-        path: '/invoices/:id',
-        name: "InvoicesEditPage",
-        component: InvoicesEditPage
-    },
-    {
-        path: 'banks',
-        name: "NewBankDetails",
-        component: NewBankDetails
-    },
-    {
-        path: '/payments/',
-        component: PaymentsLayout,
-        children: [
+{
+    path: '/statuses/',
+    name: "StatusesPage",
+    component: StatusesPage
+},
+{
+    path: '/stocks',
+    name: "StockPage",
+    component: StockPage
+},
+{
+    path: '/stocks/create',
+    name: "NewStock",
+    component: NewStock
+},
+{
+    path: '/customers',
+    name: "CustomersPage",
+    component: CustomersPage
+},
+{
+    path: '/users',
+    name: "AdvisorsPage",
+    component: AdvisorsPage
+},
+{
+    path: '/jobCards/serviceTypes',
+    name: "ServiceTypePage",
+    component: ServiceTypePage
+},
+{
+    path: '/tax',
+    name: "TaxConfigList",
+    component: TaxConfigList
+},
+{
+    path: '/invoices',
+    name: "InvoicesListPage",
+    component: InvoicesListPage
+},
+{
+    path: '/invoices/:id',
+    name: "InvoicesEditPage",
+    component: InvoicesEditPage
+},
+{
+    path: 'banks',
+    name: "NewBankDetails",
+    component: NewBankDetails
+},
+{
+    path: '/payments/',
+    component: PaymentsLayout,
+    children: [
 
 
-            {
-                path: '/estimates',
-                name: "EstimateList",
-                component: EstimateList
-            },
-            {
-                path: '/orders',
-                name: "OrderList",
-                component: OrderList
-            },
-            {
-                path: '/orders',
-                name: "NewTax",
-                component: NewTax
-            }
-        ]
-    },
-    {
-        path: '/',
-        name: 'Login',
-        component: LoginPage
-    },
-    {
-        path: '/profile',
-        component: ProfileLayout,
-        children: [{
-            path: '',
-            name: 'ViewProfile',
-            component: ViewProfile,
-        }]
-    },
-    {
-        path: '/sell',
-        name: 'SellProducts',
-        component: SellProductsPage
-    },
+        {
+            path: '/estimates',
+            name: "EstimateList",
+            component: EstimateList
+        },
+        {
+            path: '/orders',
+            name: "OrderList",
+            component: OrderList
+        },
+        {
+            path: '/orders',
+            name: "NewTax",
+            component: NewTax
+        }
+    ]
+},
+{
+    path: '/',
+    name: 'Login',
+    component: LoginPage
+},
+{
+    path: '/profile',
+    component: ProfileLayout,
+    children: [{
+        path: '',
+        name: 'ViewProfile',
+        component: ViewProfile,
+    }]
+},
+{
+    path: '/sell',
+    name: 'SellProducts',
+    component: SellProductsPage
+},
 
-    {
-        path: '/parts',
-        name: 'Parts',
-        component: PartsPage
-    },
-    {
-        path: '/checklist',
-        name: 'CheckList',
-        component: CheckListPage
-    },
-    {
-        path: '/viewchecklist',
-        name: 'ViewCheckList',
-        component: ViewCheckListPage
-    },
-    {
-        path: '/DownloadJobCard/:id',
-        name: 'DownloadJobCard',
-        component: DownloadJobCardPage
-    }
+{
+    path: '/parts',
+    name: 'Parts',
+    component: PartsPage
+},
+{
+    path: '/checklist',
+    name: 'CheckList',
+    component: CheckListPage
+},
+{
+    path: '/viewchecklist',
+    name: 'ViewCheckList',
+    component: ViewCheckListPage
+},
+{
+    path: '/DownloadJobCard/:id',
+    name: 'DownloadJobCard',
+    component: DownloadJobCardPage
+}
 ]
 
 const router = new VueRouter({
     routes,
-    mode: "history"
+    mode: "hash"
 })
 
 export default router
